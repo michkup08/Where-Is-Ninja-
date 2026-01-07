e1e4c364120095df4ddbea60995c35979ea0ddc6
Gaikk <gaik.k02@gmail.com>
2026-01-07 11:47:42 +0100

    ending

--- Assets/scripts/MainMenu.cs
+++ Assets/scripts/MainMenu.cs
@@ -7,7 +7,7 @@ public class MainMenu : MonoBehaviour
 {
     public void StartGame()
     {
-        SceneManager.LoadScene("Level1");
+        SceneManager.LoadScene("Level Intro");
     }
 
     public void QuitGame()
